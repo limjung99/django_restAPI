3f8bd338e1ee3abf127accaa160dd27de674def5
lim <limjung99@naver.com>
2023-05-05 15:35:25 +0900

    view update

--- polls/urls.py
+++ polls/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
+app_name = "polls"
 urlpatterns = [
     #꺾쇠부분은 capture되어 view의 arg와 일치하는 패턴을 식별한다
     #즉 view의 인수가 question_id와 일치하는 패턴을 식별하여 매칭되는 view를 호출
--- polls/views.py
+++ polls/views.py
@@ -1,14 +1,15 @@
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 from .models import Question
 from django.template import loader
+from django.shortcuts import render,get_object_or_404
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    template = loader.get_template("polls/index.html")
     context = {
         "latest_question_list": latest_question_list,
     }
-    return HttpResponse(template.render(context,request))
+    return render(request,"polls/index.html",context)
+
 """
 render를 이용해서 이렇게 짧게도 index view 생성가능 
 def index(request):
@@ -16,8 +17,10 @@ def index(request):
     context = {"latest_question_list": latest_question_list}
     return render(request, "polls/index.html", context)
 """
+
 def detail(request, question_id):
-    return HttpResponse("You are looking at question %s" %question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,"polls/detail.html",{"question":question})
 
 def results(requst, question_id):
     response = "You are looking at the result of question %s"
