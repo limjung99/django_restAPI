a17d91ec69c850144be9f2fe4181e599ea02138c
lim <limjung99@naver.com>
2023-05-13 01:38:38 +0900

    simple-jwt token생성 로직

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,9 +6,11 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
+    #apps
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
     path("mytodos/",include("mytodos.urls")),
+    #Token
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- mytodos/views.py
+++ mytodos/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponse
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from .models import Users
+
 # Create your views here.
 
 # Login을 담당 view <- API view를 상속받는 class형 view (basci Django view는 함수)
@@ -16,23 +17,30 @@ class LoginView(APIView):
     def post(self,request):
         serializer = UsersSerializer(data = request.data)
         if serializer.is_valid(): # post로 전송받은 data의 유효성 검사 
-            #직렬화된 유효한 data를 validated_data 메소드로 가져온다. 
-            id = serializer.validated_data['user_id']
-            pw = serializer.validated_data['user_pw']
             # DB에 일치하는 tuple이 존재하는지검사 
             if serializer.validate(request.data):
-                #simple-jwt 
-                user = Users.objects.filter(user_id=id,user_pw=pw)
+                #DB에 존재하는 tuple이므로 token을 발급해준다
+
+                #직렬화된 유효한 data를 validated_data 메소드로 가져온다. 
+                id = serializer.validated_data['user_id']
+                pw = serializer.validated_data['user_pw']
+                #filter를 통해 iterable한 tuple collection을 가져온다 , index로도 접근가능.
+                users = Users.objects.filter(user_id=id,user_pw=pw)
+                user = users[0]
+                #token생성 로직 
                 token = TokenObtainPairSerializer.get_token(user)
                 refresh_token = str(token)
                 access_token = str(token.access_token)
+                res = Response({
+                    "token":{
+                        "access":access_token,
+                        "refresh":refresh_token,
+                    },
+                },status=status.HTTP_200_OK)
+                res.set_cookie("access",access_token,httponly=True)
+                res.set_cookie("refresh",refresh_token,httponly=True)
 
-                return Response({
-                    "jwt_token" : {
-                        "refresh_token" : refresh_token,
-                        "access_token" : access_token
-                    }
-                })
+                return res
         else:
             return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
         
